9e5ce547580c6f959435372f9053b4b6d72938d9
Mads Røjkum <roejkum@gmail.com>
2019-01-28 14:41:52 +0100

    contact form: Form validation and further styling

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -2,140 +2,172 @@ import React from "react";
 import Layout from '../../components/Layout/Layout'
 import addToMailchimp from 'gatsby-plugin-mailchimp';
 
-// function encode(data) {
-//   return Object.keys(data)
-//     .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-//     .join("&");
-// }
-
 export default class Index extends React.Component {
   constructor(props) {
     super(props);
     this.state = { 
       isValidated: false, 
+      emailValidated: false,
+      nameValidated: false,
+      errorMsg: '',
       form: {
+        email: '',
         listFields: {
-        }
+          FNAME: '',
+          LNAME: ''        }
       },
-      re: /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     };
   }
 
-  handleChange = e => {
-    const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  componentDidUpdate() {
+    console.log(this.state);
+  }
 
-    if(e.target.name === 'email') {
+  validateEmail = email => {
+    const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+  }
+
+  handleChange = e => {
+    if(e.target.name === 'email' && this.validateEmail(e.target.value)) {
       const currentFormData = this.state.form;
+      const nameValidated = this.state.nameValidated;
       this.setState({
         form: {
           ...currentFormData,
           [e.target.name]: e.target.value 
-        }
+        },
+        emailValidated: true,
+        isValidated: nameValidated
       });
+      e.target.className = 'valid input';
     }
 
-    if(e.target.name === 'name') {
-      const input = e.target.value;
-      console.log('input: ', input);
+    if(e.target.value && e.target.name !== 'email') {
+      const currentForm = this.state.form;
       const currentListFields = this.state.form.listFields;
-      const newListFields = {
-        ...currentListFields,
-        [e.target.name]: input,
-        poop: 'poop'
-      }
+      const emailValidated = this.state.emailValidated;
+      const newForm = {
+        ...currentForm,
+        listFields: {
+          ...currentListFields,
+          [e.target.name]: e.target.value
+        }
+      };
       this.setState(() => {
         return {form: {
-          listFields: {
-            ...newListFields
-          }
-        }};
+          ...newForm
+        },
+        isValidated: emailValidated,
+        nameValidated: true
+      };
       });
+      e.target.className = 'valid input';
     }
 
     if(!e.target.value && e.target.name !== 'email') {
       e.target.className = 'invalid input';
+      this.setState({
+        isValidated: false
+      });
     }
-    if(e.target.name === 'email' && !re.test(e.target.value)) {
+    if(e.target.name === 'email' && !this.validateEmail(e.target.value)) {
       e.target.className = 'invalid input';
+      this.setState({
+        isValidated: false,
+        emailValidated: false
+      })
     }
-    if(e.target.name === 'email' && re.test(e.target.value)) {
-      e.target.className = 'valid input';
-    }
-    if(e.target.value && e.target.name !== 'email') {
-      e.target.className = 'valid input';
-    }
-
-    //global validation
-    
-    console.log(this.state.form);
   };
-
-
+  
   handleSubmit = e => {
     e.preventDefault();
 
-    if(this.state.form.email)
-    addToMailchimp(this.state.form.email, this.state.form.listFields)
-    // listFields are optional if you are only capturing the email address.
-    .then(data => {
-      // I recommend setting data to React state
-      // but you can do whatever you want (including ignoring this `then()` altogether)
-      console.log('data: ', data);
-    })
-    // .catch(() => {
-    //   // unnecessary because Mailchimp only ever
-    //   // returns a 200 status code
-    //   // see below for how to handle errors
-    // })
-    console.log('submitted');
+    if(this.state.isValidated) {
+      addToMailchimp(this.state.form.email, this.state.form.listFields)
+        .then(data => {
+        // I recommend setting data to React state
+        // but you can do whatever you want (including ignoring this `then()` altogether)
+        if(data.result === 'error') {
+          this.setState({errorMsg: 'Der skete en fejl.'})
+        }
+      })
+      console.log('submitted');
+    } else {
+      // this.setState({showError: true});
+      this.setState({errorMsg: 'Der er fejl i din indtastning. Gennemgå venligst formularen og prøv igen.'})
+    }
   };
 
   render() {
 
-    if(this.state.re.test(this.state.form.email) && this.state.form.listFields.name) {
-      console.log('formularen er udfyldt og mailen er korrekt');
+    const errorMsg = null;
+
+    if(this.state.errorMsg) {
+      this.errorMsg = (
+        <p className="error">{this.state.errorMsg}</p>
+      );
     }
+
 
     return (
       <Layout>
         <section className="section">
           <div className="container">
-            <div className="content">
-        <h1>Contact</h1>
-        <form
-          name="contact"
-          method="post"
-          action="/contact/thanks/"
-          onSubmit={this.handleSubmit}
-        >
-          {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
-          <input type="hidden" name="form-name" value="contact" />
-          <div hidden>
-            <label>
-              Don’t fill this out:{" "}
-              <input name="bot-field" onChange={this.handleChange} />
-            </label>
-          </div>
-          <div className="field">
-            <label className="label" htmlFor={"name"} hidden>navn</label>
-            <div className="control">
-              <input className="input" type={"text"} name={"name"} onChange={this.handleChange} onFocus={this.handleChange} id={"name"} placeholder="navn" required={true} />
-              <p>Fejl i indtastning</p>
+            <div className="row">
+              <div className="col-xs-12">
+                <h2>Skriv under nu!</h2>
+              
+          <form
+            name="contact"
+            method="post"
+            action="/contact/thanks/"
+            onSubmit={this.handleSubmit}
+            >
+            <div className="row">
+            {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+            <input type="hidden" name="form-name" value="contact" />
+            <div hidden>
+              <label>
+                Don’t fill this out:{" "}
+                <input name="bot-field" onChange={this.handleChange} />
+              </label>
+            </div>
+            <div className="container">
+              <div className="row">
+                <div className="field col-xs-6">
+                  <label className="label" htmlFor={"FNAME"} hidden>Fornavn</label>
+                    <div className="control">
+                      <input className="input" type={"text"} name={"FNAME"} onChange={this.handleChange} onFocus={this.handleChange} id={"name"} placeholder="Fornavn" required={true} />
+                      <p>Fejl i indtastning</p>
+                    </div>
+                  </div>
+                  <div className="field col-xs-6">
+                    <label className="label" htmlFor={"FNAME"} hidden>Efternavn</label>
+                    <div className="control">
+                      <input className="input" type={"text"} name={"LNAME"} onChange={this.handleChange} onFocus={this.handleChange} id={"name"} placeholder="Efternavn" required={true} />
+                      <p>Fejl i indtastning</p>
+                    </div>
+                  </div>
+                <div className="field col-xs-12">
+                  <label className="label" htmlFor={"email"} hidden>Email</label>
+                    <div className="control">
+                      <input className="input" type={"email"} name={"email"} onChange={this.handleChange} onFocus={this.handleChange} id={"email"} placeholder="Email" required={true} />
+                      <p>Fejl i indtastning</p>
+                    </div>
+                  </div>
+                  <div className="field col-xs-12">
+                    <button className="button is-link" type="submit" disabled={!this.state.isValidated}>Send</button>
+                    {errorMsg}
+                    {this.errorMsg}
+                  </div>
+                </div>
+              </div>
+              </div>
+            </form>
+            </div>
             </div>
           </div>
-          <div className="field">
-            <label className="label" htmlFor={"email"} hidden>Email</label>
-              <div className="control">
-                <input className="input" type={"email"} name={"email"} onChange={this.handleChange} onFocus={this.handleChange} id={"email"} placeholder="Email" required={true} />
-                <p>Fejl i indtastning</p>
-              </div>
-          </div>
-          <div className="field">
-            <button className="button is-link" type="submit">Send</button>
-          </div>
-        </form>
-        </div>
-        </div>
         </section>
       </Layout>
     );
