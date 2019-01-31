93d95ee227be343d3e5cf9d7e4c5070c879f0797
Mads Røjkum <roejkum@gmail.com>
2019-01-31 11:50:43 +0100

    adding env variable, updating lambda function

--- src/functions/getSubscriberAmount.js
+++ src/functions/getSubscriberAmount.js
@@ -1,6 +1,36 @@
-exports.handler = function(event, context, callback) {
-    callback(null, {
-    statusCode: 200,
-    body: "Hello, World"
-    });
-}
+const axios = require('axios');
+require('dotenv').config();
+
+exports.handler = function signUp(event, context, callback) {
+    const mcPassword = process.env.API_KEY;
+    if (!mcPassword) {
+        console.error('No MailChimp API Key include in environment variables');
+        process.exit(1);
+      }
+
+    const mcUsername = 'anystring';
+
+    axios({
+        method: 'get',
+        url: 'https://us18.api.mailchimp.com/3.0/lists/6aa8a91bc2/members?count=1',
+        auth: {
+            username: mcUsername,
+            password: mcPassword
+        }
+      })
+      .then(response =>
+        callback(null, {
+          statusCode: 200,
+          body: JSON.stringify({ msg: response.data.total_items }),
+        }),
+      )
+      .catch(() => {
+        callback(null, {
+          statusCode: 500,
+          body: JSON.stringify({
+            msg: `Kunne ikke hente data.`,
+          }),
+        });
+    });   
+  }
+  
