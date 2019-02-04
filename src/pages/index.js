2cf9c12dee2869e63d60a83371ead144b80a622b
Mads Røjkum <roejkum@gmail.com>
2019-02-04 15:32:35 +0100

    Styling changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
-import React from 'react'
-import Layout from '../components/Layout/Layout'
+import React from 'react';
+import Layout from '../components/Layout/Layout';
 import BarCount from '../components/BarCount/BarCount';
 import ContactForm from '../components/ContactForm/ContactForm';
-import logo from "../../static/img/Logo_2.svg"
-import graes from "../../static/img/Graes.svg"
+import logo from "../../static/img/logo.svg";
+import graes from "../../static/img/Graes.svg";
+import SunSvg from '../components/SunSvg/SunSvg';
+import Trees from '../components/Trees/Trees';
 
 export default class IndexPage extends React.Component {
   state = {
@@ -29,13 +31,15 @@ export default class IndexPage extends React.Component {
     return (
       <Layout>
         <section className="section section-signup">
+        <SunSvg/>
+        <Trees/>
           <div className="container-fluid wrap">
             <div className="row">
               {/* tekst */}
-              <div className="col-sm-7 col-md-6 col-md-offset-1 col-xs-12 last-sm">
-                <img src={logo} alt="Logo" />
-                <h1 className="white-text text-center-xs">Kan vi få 100.000 underskrifter på at gøre <span className="bold-text">valget grønt?</span></h1>
-                <p className="white-text text-center-xs">Hvis du også synes at klima og natur skal øverst på dagsordenen til det kommende folketingsvalg, så skriv under og vær med til at råbe politikerne op!</p>
+              <div className="col-sm-7 col-md-6 col-xs-12 last-sm pl-5-sm">
+                <img src={logo} alt="Logo" className="logo"/>
+                <h1>Kan vi få 100.000 underskrifter på at gøre <span className="bold-text">valget grønt?</span></h1>
+                <p>Hvis du også synes at klima og natur skal øverst på dagsordenen til det kommende folketingsvalg, så skriv under og vær med til at råbe politikerne op!</p>
               </div>
 
                 {/* Signupform */}
