519fbadfaaab92e50a41830d17706d9542b8dde0
Mads Røjkum <roejkum@gmail.com>
2019-02-18 15:02:32 +0100

    Add cookie functionality

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,46 +1,75 @@
-import React from 'react'
-import Helmet from 'react-helmet'
-import { StaticQuery, graphql } from "gatsby"
+import React, { Component } from 'react';
+import Helmet from 'react-helmet';
+import { StaticQuery, graphql } from "gatsby";
 
 import './sass/main.scss'
+import CookieConsent from '../cookieConsent/cookieConsent';
 
-const TemplateWrapper = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query HeadingQuery {
-          site {
-            siteMetadata {
-              title,
-              description,
+class TemplateWrapper extends Component {
+  state = {
+    analyticsAccepted: false
+  }
+
+  acceptAnalyticsHandler = () => {
+    console.log('clicked');
+    this.setState({analyticsAccepted: true});
+  }
+
+  render() {
+    return (
+
+      <StaticQuery
+      query={graphql`
+        query HeadingQuery {
+            site {
+              siteMetadata {
+                title,
+                description,
+              }
             }
           }
-        }
-    `}
-    render={data => (
-      <>
-        <Helmet>
-          <html lang="da" />
-          <title>{data.site.siteMetadata.title}</title>
-          <meta name="description" content={data.site.siteMetadata.description} />
-          
-          <link rel="apple-touch-icon" sizes="180x180" href="/img/apple-touch-icon.png" />
-	        <link rel="icon" type="image/png" href="/img/favicon-32x32.png" sizes="32x32" />
-	        <link rel="icon" type="image/png" href="/img/favicon-16x16.png" sizes="16x16" />
-	
-	        <link rel="mask-icon" href="/img/sol-solo.svg" color="#80B35B" />
-          {/* <link rel="mask-icon" href="/img/safari-pinned-tab.svg" color="#ff4400" /> */}
-	        <meta name="theme-color" content="#80B35B" />
+      `}
+      render={data => (
+        <>
+          <Helmet>
+            <html lang="da" />
+            <title>{data.site.siteMetadata.title}</title>
+            <meta name="description" content={data.site.siteMetadata.description} />
+            
+            <link rel="apple-touch-icon" sizes="180x180" href="/img/apple-touch-icon.png" />
+            <link rel="icon" type="image/png" href="/img/favicon-32x32.png" sizes="32x32" />
+            <link rel="icon" type="image/png" href="/img/favicon-16x16.png" sizes="16x16" />
+    
+            <link rel="mask-icon" href="/img/sol-solo.svg" color="#80B35B" />
+            {/* <link rel="mask-icon" href="/img/safari-pinned-tab.svg" color="#ff4400" /> */}
+            <meta name="theme-color" content="#80B35B" />
+  
+            <meta property="og:type" content="business.business" />
+            <meta property="og:title" content={data.site.siteMetadata.title} />
+            <meta property="og:url" content="/" />
+            <meta property="og:image" content="/img/mosaik.jpg" />
+            
+            {this.state.analyticsAccepted ? (
+              <script>{(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-N55QRBM')}</script> 
+            ) : ''}
+          </Helmet>
+          {/* {this.state.analyticsAccepted ? (
+              <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N55QRBM"
+              height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript> 
+            ) : ''} */}
+                    
+          <CookieConsent acceptAnalytics={this.acceptAnalyticsHandler}/>
+          {this.props.children}
+        </>
+      )}
+    />
 
-	        <meta property="og:type" content="business.business" />
-          <meta property="og:title" content={data.site.siteMetadata.title} />
-          <meta property="og:url" content="/" />
-          <meta property="og:image" content="/img/mosaik.jpg" />
-        </Helmet>
-        {/* <Navbar /> */}
-        {children}
-      </>
-    )}
-  />
-)
+    );
+  }
+  }
 
 export default TemplateWrapper
