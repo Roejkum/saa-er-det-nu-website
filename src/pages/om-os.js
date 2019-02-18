632abb8cb9fc29ceff3f7d69677f6e97166f82b5
Malthe Blichfeldt <maltheblichfeldt@gmail.com>
2019-02-19 00:34:25 +0100

    about page

---
+++ src/pages/om-os.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import Layout from '../components/Layout/Layout';
+import SunSvg from '../components/SunSvg/SunSvg';
+
+const cookiePolicy = () => (
+
+  <Layout>
+    <section className="section section-signup fullHeight">
+      <SunSvg/>
+      <div className="container-fluid wrap">
+        <div className="row">
+          <div className="col-sm-8 col-sm-offset-2 col-xs-12">
+            <div className="container-fluid white-box fade-in">
+              <div className="row">
+                <div className="col-xs-12">
+                <Link to="/">&larr; Til forsiden</Link>
+                  <h2>Om ‘Så er det nu: Gør valget grønt’</h2>
+                  <p>Formålet med initiativet ‘Så er det nu: Gør valget grønt’ er at gøre det kommende folketingsvalg grønt. 
+                  Sammen med alle de borgere der vil, vil vi sætte klima, natur og miljø øverst på den politiske dagsorden. <br /><br />
+                  Vi mener, at politikerne skal forpligte sig på at arbejde for grønne, langsigtede løsninger på klimaudfordringerne. 
+                  Derfor har vi startet en kampagne frem mod folketingsvalget, som skal gøre det let at engagere 
+                titusinder af mennesker i arbejdet med at præge den politiske diskussion i en mere grøn retning.<br /><br />
+                  Vi mener, at vi her og nu skal iværksætte den nødvendige omstilling af vores samfund, 
+                  for at undgå at den globale opvarmning overstiger 1,5 grader. 
+                  Vi skal sikre at vores produktion og forbrug sker i fuld balance med naturgrundlag, 
+                  klima og dyreliv. Danmark skal gå først i den grønne omstilling og vise, at det er muligt at finde de nye løsninger. 
+                  Vi skal beskytte vores vand - både vores drikkevand og verdenshavene. Og vi skal bevare et stabilt klima 
+                  og varieret dyre- og planteliv. Vi skal sørge for, at de næste generationer også kan nyde den summende humlebi,
+                   den sky hare og havørnens brede vingefang.
+                  </p>
+                  <br />
+                  <h3>Hvem er vi? ...altså ud over dig :)</h3>
+                  <p>Så er det nu-initiativet drives af frivillig arbejdskraft som et åbent netværk og er ikke knyttet til noget politisk parti. 
+                  Alle der kan bakke om initiativets mål er inviteret med til at tage del i vores aktiviteter. Hvis det der er dig, 
+                  så kan du hurtigt komme med ved at skrive under og gribe de invitationer til handling, du får i e-mails fra ‘Så er det nu’.<br/><br/>
+                  Nedenfor kan du se, hvem der har startet det hele, for at invitere dig med.
+                  </p> 
+                  <br />
+                  <h3>Anbefalere af underskriftsindsamlingen:</h3>
+                  <p>
+<b>Susanne Mieth Andersen</b>  Pensionist; <b>Annie Nicolaisen</b> freelance journalist/pensionist; <b>Gitte Ladving</b> Fleksjob med rengøring; <b>Marie Chimwemwe Degnbol</b> Konsulent, foredragsholder og facilitator; <b>Monika Lillelund Dybro Olsen</b> Sygemeldt; <b>Flemming Torsten Sørensen</b> Idrætskonsulent; <b>Vagn Kjær-Hansen</b> Elektroingeniør; <b>Birthe Rosenfeldt</b> Dramaturg; <b>Cathrine Hartung</b> Business developer; <b>Jessie Seehuusen</b> Pensionist, tidligere pædagog; <b>Henning Lund</b> Radiooperatør; <b>Lars Ravn-Jonsen</b> Lektor; <b>Hanne Wind-Larsen</b> Biolog; <b>Anne Marie Kragholm Skovsgaard  </b> Geograf; <b>Aske Skovmand Bosselmann</b> Adjunkt; <b>Gitte Lyager Ryan</b> Miljø & ressource management;<b>Charlotte Detlif</b> Erhvervscoach; <b>Britta Glinvad Kristensen</b> Aktivitetsmedarbejder ved Frederikshavn kommune; <b>Bjørn Tving Stauning</b> Fotograf; <b>Otto og Elisabeth Rasmussen</b> Landmænd og butiksejere; <b>Tyge Mortensen</b> Højskoleforstander og fremtidsforsker; <b>Sju G. Thorup</b> selvstændig managementkonsulent; <b>Tenna Helene Malberg</b> Pensioneret kontorfuldmægtig; <b>Signe Petersen</b> Gartner; <b>Casper Schmedes</b> Gymnasieelev; <b>Stine Schaksen</b> Folkeskolelærer; <b>Anne Kirstine Feilberg</b> Lærer; <b>Trine Ritz Andersson</b> Ernæringsvejleder; <b>Troels Grene</b> Forretningsudvikler i en start-up; <b>Louise Stisen Holm</b> Studerende; <b>Marianne Søholm Iversen</b> Psykolog; <b>Birgitte Jensen</b> pensionist/ tidl. lærer; <b>Anne Line Trøllund Nielsen</b> Museumsformidler; <b>Mia Andrea Petersen </b> Studerende; <b>Lasse Baaner</b> Jurist; <b>Frederikke Oldin</b> Sprogpsykolog; <b>Anne Sofie Pinstrup Jørgensen</b> Barsel; <b>Bettina Bach</b> Boligsocial medarbejder; <b>Lisbeth Flink Nielsen</b> Lærer; <b>Knud Anker Iversen </b> Folkepensionist; <b>Louise Zabel </b> Webkoordinator; <b>Lone Belling</b> Forfatter, foredragsholder og konsulent; <b>Natasha Sachiko Matsushima Carstens</b> Projektleder Københavns kommune; <b>Ulla Schønberg</b> Sygeplejske; <b>Thomas Malthesen Hiorth</b> Landskabsforvalter; <b>Anne Lise Ernst</b> pensionist; <b>Jenny Elisabeth Malberg</b> Sygeplejerske; <b>Christian Ege</b> Seniorkonsulent; <b>Frederik Roland Sandby</b> Studerende; <b>Rasmus Skov Larsen</b> Miljø- og formidlingskonsulent; <b>Ebbe Steen Hermansen</b> Selvstændig skov - og Landskabsingeniør; <b>Charlotte Kirstine Gunnarsen</b> Fuldmægtig i Gældsstyrelsen; <b>Anne Moloney</b> permakulturdesigner og mindfulnessformidler; <b>Birte Christensen</b> Pensionist; <b>Dorte Gustafsson </b> pensioneret lærer; <b>Malene Holmberg Brandt</b> Familieplejer og selvstændig; <b>Ole Dedenroth</b> Højskoleforstander; <b>Niels-Andrè Benn</b> Førtidspensionist; <b>Esben Pedersen</b> pædagogmedhjælper; <b>Trine Hald Degnbol</b> Efterskoleelev; <b>Frank Hansen</b> Formand for Nyborg sportsfiskerforening; <b>Malaika Thomsen</b> Autoriseret Psykolog; <b>Tina Kristensen</b> Grafisk tekniker; <b>Kirsten Gullmaj Nielsen</b> Pensionist, tidligere lærer; <b>Sten Hartung</b> Sognepræst; <b>Søren Brøndum Christensen</b> Bredbåndskoordinator; <b>Lene Dahl</b> Underviser; <b>Rebecca Kaagh Nordbo</b> Højskolelærer på Vestjylland Højskole; <b>Camilla Nielsen-Englyst</b> Konsulent; <b>Lise Justesen</b> Lektor; <b>Karina Finch Schandorf Petersen</b> Have- og Parkingeniør; <b>Irene Schjødtz Bæksgaard </b> Key Account Manager; <b>Hester Callaghan</b> Landmand; <b>Dorthe Weiergang</b> Forhenværende overlærer; <b>Anders Hollen Nielsen</b> Lærer; <b>Karsten Dahl Pedersen</b> Socialpædagog; <b>Peter Johns</b> Musiker; <b>Caroline Diget</b> Gymnasieelev; <b>Armin Vauk</b> Selvstændig; <b>Kjeld Hansen</b> Økologisk landmand og journalist; <b>Marcel Bragt</b> HF studerende; <b>Pia Hansen</b> Efterløn, tidligere socionom; <b>Peter Velschow Bang-See</b> IT-supporter; <b>Rasmus Madsen</b> Gymnasieelev; <b>Philip Hahn-Petersen</b> Naturrådgiver; <b>Torben Struck</b> Postarbejder; <b>Erik Sørensen</b> Flyoperatør; <b>Dan Boding-Jensen</b> Business manager; <b>Gunner Poulsen</b> Lærer; <b>Carina Keisen Bruhn </b> Frisør; <b>Casper Grane</b> Social- og sundhedsassistent; <b>Hans Grishauge</b> Pens. sekretariatsleder for Vartov/Grundtvigsk Forum; <b>Poul Engholm</b> Sygeplejerske; <b>Gorm Jensen</b> Maskinmester/pensionist; <b>Bjørn Simonsen Pelle</b> Pensioneret elektrier; <b>Mette Margrethe Sørensen </b> Lærer; <b>Niels Peter Hvillum</b> Selvstændig; <b>Maya Bjerrum Trinkjær</b> Underviser; 
+
+                  </p>
+                  <br/>
+                  <br/>
+                  <h3>Følgende personer har været med i den opstartende fase og udgør stifterne og den koordinerende gruppe i initiativet:</h3>
+                  <p>
+                  Bag initiativet står en række engagerede borgere med blandet baggrund.
+                   Nogle er forskere, andre kampagne- og organisationsfolk. 
+                   Fælles for os er, at vi er bekymrede over klodens klima og gerne 
+                   vil være med til at gøre en forskel.
+                   </p>
+               
+             
+                   <p>
+                   <li> Pil Christensen</li>
+                   <li> Rune Baastrup</li>
+                   <li> Carsten Lunding </li>
+                   <li> Liv Detlif</li>
+                   <li> Stefan Gaarsmand Jacobsen </li>
+                   <li>Jens Friis Lund</li>
+                   <li>Alexander Thorvaldsen </li>
+                   <li>Mattias Friis Jørgensen </li>
+                   <li> Kia Ditlevsen</li>
+                   <li> Morten Wendler Jørgensen</li>
+                   <li> Andreas Lloyd</li>
+                   <li> Lasse Folke Henriksen</li>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </Layout>
+);
+
+export default cookiePolicy;
