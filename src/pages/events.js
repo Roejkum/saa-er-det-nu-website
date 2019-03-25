e76f5606c2224ba1452c10255821446cd2d62724
Mads Røjkum <roejkum@gmail.com>
2019-03-25 12:23:53 +0100

    Adding events page

---
+++ src/pages/events.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Layout from '../components/Layout/Layout';
+import SunSvg from '../components/SunSvg/SunSvg';
+import { Link } from 'gatsby';
+import { Helmet } from 'react-helmet';
+
+const events = () => (
+
+  <Layout>
+      <Helmet>
+      <title>'Så er det nu: Gør valget grønt’ - events</title>
+    </Helmet>
+    <section className="section section-signup fullHeight">
+      <SunSvg/>
+      <div className="container-fluid wrap">
+        <div className="row">
+          <div className="col-sm-8 col-sm-offset-2 col-xs-12">
+            <div className="container-fluid white-box fade-in">
+              <div className="row">
+                <div className="col-xs-12">
+                <script src='https://actionnetwork.org/widgets/v3/event_campaign/middagstest?format=js&source=widget&style=full'></script><div id='can-event_campaign-area-middagstest' style={{width: '100%'}}></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </Layout>
+);
+
+export default events;
