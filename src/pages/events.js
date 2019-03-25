d3dda2e9580f1838f90dc6f0bc140a34154acdb8
Mads Røjkum <roejkum@gmail.com>
2019-03-25 13:33:34 +0100

    events page: styling and adding layout css from AN

--- src/pages/events.js
+++ src/pages/events.js
@@ -18,7 +18,7 @@ const events = () => (
             <div className="container-fluid white-box fade-in">
               <div className="row">
                 <div className="col-xs-12">
-                <script src='https://actionnetwork.org/widgets/v3/event_campaign/middagstest?format=js&source=widget&style=full'></script><div id='can-event_campaign-area-middagstest' style={{width: '100%'}}></div>
+                <link href='https://actionnetwork.org/css/style-embed-whitelabel-v3.css' rel='stylesheet' type='text/css' /><script src='https://actionnetwork.org/widgets/v3/event_campaign/middagstest?format=js&source=widget&style=full'></script><div id='can-event_campaign-area-middagstest' style={{width: '100%'}}></div>
                 </div>
               </div>
             </div>
