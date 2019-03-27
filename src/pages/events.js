9fdd6a01d9c57b68ec27b303aeb83e234bd9a013
Malthe Blichfeldt <maltheblichfeldt@gmail.com>
2019-03-27 22:32:54 +0100

    MB change

--- src/pages/events.js
+++ src/pages/events.js
@@ -1,31 +1,63 @@
+// import React from 'react';
+// import Layout from '../components/Layout/Layout';
+// import SunSvg from '../components/SunSvg/SunSvg';
+// import { Link } from 'gatsby';
+// import { Helmet } from 'react-helmet';
+
+// const events = () => (
+
+//   <Layout>
+//       <Helmet>
+//       <title>'Så er det nu: Gør valget grønt’ - events</title>
+//     </Helmet>
+//     <section className="section section-signup fullHeight">
+//       <SunSvg/>
+//       <div className="container-fluid wrap">
+//         <div className="row">
+//           <div className="col-xs-12">
+//             <div className="container-fluid fade-in">
+//               <div className="row">
+//                 <div className="col-xs-12">
+//                 <link href='https://actionnetwork.org/css/style-embed-whitelabel-v3.css' rel='stylesheet' type='text/css' /><script src='https://actionnetwork.org/widgets/v3/event_campaign/middagstest?format=js&source=widget&style=full'></script><div id='can-event_campaign-area-middagstest' style={{width: '100%'}}></div>
+//                 </div>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </section>
+//   </Layout>
+// );
+
+// export default events;
+
 import React from 'react';
 import Layout from '../components/Layout/Layout';
 import SunSvg from '../components/SunSvg/SunSvg';
 import { Helmet } from 'react-helmet';
 
-const events = () => (
+// const events = () => (
 
-  <Layout>
+//   <Layout>
+//       <Helmet>
+//       <title>'Så er det nu: Gør valget grønt’ - events</title>
+//     </Helmet>
+//     <Events/>
+//   </Layout>
+// );
+
+// export default events;
+
+
+function events () {
+  return (
+    <Layout>
       <Helmet>
-      <title>'Så er det nu: Gør valget grønt’ - events</title>
-    </Helmet>
-    <section className="section section-signup fullHeight">
-      <SunSvg/>
-      <div className="container-fluid wrap">
-        <div className="row">
-          <div className="col-xs-12">
-            <div className="container-fluid fade-in">
-              <div className="row">
-                <div className="col-xs-12">
-                <link href='https://actionnetwork.org/css/style-embed-whitelabel-v3.css' rel='stylesheet' type='text/css' /><script src='https://actionnetwork.org/widgets/v3/event_campaign/middagstest?format=js&source=widget&style=full'></script><div id='can-event_campaign-area-middagstest' style={{width: '100%'}}></div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  </Layout>
-);
+        <title>'Så er det nu: Gør valget grønt’ - events</title>  
+        </Helmet>
+      <Events/>
+    </Layout>
+  )
+}
 
 export default events;
