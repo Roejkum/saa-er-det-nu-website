be00b3c9306d90a06fdfff69a1c734dd4be8e6ea
Mads Røjkum <roejkum@gmail.com>
2019-03-27 21:15:06 +0100

    Change contact form to class

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,11 +1,35 @@
-import React, { Component } from 'react';
+import React from 'react';
 import addToMailchimp from 'gatsby-plugin-mailchimp';
 import { Link } from 'gatsby';
 
-const ContactForm = () => (
-  <>
-    <link href='https://actionnetwork.org/css/style-embed-whitelabel-v3.css' rel='stylesheet' type='text/css' /><script src='https://actionnetwork.org/widgets/v3/form/gor-valget-gront?format=js&source=widget&style=full'></script><div id='can-form-area-gor-valget-gront' style={{width: '100%'}}></div>
-  </>
-)
+class ContactForm extends React.Component  {  
+  constructor(props) {
+    super(props);
+    this.state = { 
+      mounted: false
+    }; 
+  } 
+
+  componentDidMount() {
+    this.setState({
+      mounted: true
+    })
+  }
+       
+  render() {
+    
+    let form = <p>Indlæser</p>;;
+
+    if(this.state.mounted) {
+      form = <div><link href='https://actionnetwork.org/css/style-embed-whitelabel-v3.css' rel='stylesheet' type='text/css' /><script src='https://actionnetwork.org/widgets/v3/form/gor-valget-gront?format=js&source=widget&style=full'></script><div id='can-form-area-gor-valget-gront' style={{width: '100%'}}></div></div>;
+    } 
+
+    return (
+      <div>
+        { form }
+      </div>
+    )
+  }
+}
 
 export default ContactForm;
