9abc48a6d7950b1a147e8eebe0f45ad85f0a994f
Malthe Blichfeldt <maltheblichfeldt@gmail.com>
2019-04-23 15:01:05 +0200

    cookie i samme mappe som go daddy

---
+++ src/pages/.well-known/pki-validation/cookie-politik.js
@@ -0,0 +1,161 @@
+import React from 'react';
+import Layout from '../components/Layout/Layout';
+import SunSvg from '../components/SunSvg/SunSvg';
+import { Link } from 'gatsby';
+import { Helmet } from 'react-helmet';
+
+
+const cookiePolicy = () => (
+
+  <Layout>
+    <Helmet>
+      <title>Cookie-politik for Så er det nu’s hjemmesider</title>
+    </Helmet>
+    <section className="section section-signup fullHeight">
+      <SunSvg/>
+      <div className="container-fluid wrap">
+        <div className="row">
+          <div className="col-sm-8 col-sm-offset-2 col-xs-12">
+            <div className="container-fluid white-box fade-in">
+              <div className="row">
+                <div className="col-xs-12">
+                <Link to="/">&larr; Til forsiden</Link>
+                  <h2>Cookie-politik for Så er det nu’s hjemmesider</h2>
+                  <p className="small">11. Februar. 2019</p>
+                  <p>Vi bruger cookies på Så er det nu’s hjemmesider. Cookies er nødvendige for at få hjemmesiden til at fungere, men de giver også information om, hvordan du bruger siden, så vi løbende kan forbedre den og vores øvrige arbejde. Cookies på Så er det nu’s hjemmesider bruges til optimering af indholdet, trafikmåling fra Google Analytics, og integration af sociale tjenester som Facebook og indlejrede tjenester.</p>
+                  <p>På siden her kan du se hvilke typer af cookies, vi anvender, hvor de kommer fra, og hvordan du evt. kan fravælge dem.</p>
+
+                  <h3>Oversigt:</h3>
+                  <ol>
+                    <li>Hvad er cookies?</li>
+                    <li>Sådan sletter eller blokerer du for cookies</li>
+                    <li>Typer af cookies på vores hjemmesider
+                      <ul>
+                        <li>Systemets egne cookies</li>
+                        <li>Trafikmåling og målretning</li>
+                        <li>Sociale medier</li>
+                        <li>Indlejrede tjenester</li>
+                      </ul>
+                    </li>
+                    <li>Sådan afviser eller sletter du dine cookies</li>
+                    <li>Oversigt over Cookies på www.saaerdet.nu</li>
+                    <li>Ejeroplysninger</li>
+                  </ol>
+
+                  <h3>1. Hvad er cookies?</h3>
+                  <p>En cookie er en lille tekstfil, der lagres på din computer eller mobiltelefon, når du besøger hjemmesider. Formålet er at kunne genkende enheden. Der er ingen personlige oplysninger gemt i vores cookies, og de kan ikke indeholde virus. Læs mere om cookies: <a href="http://minecookies.org/information-om-cookies/" target="blank">http://minecookies.org/information-om-cookies/</a></p>
+
+                  <h3>2. Sådan sletter eller blokerer du for cookies</h3>
+                  <p>Som bruger kan du selv bestemme, om du vil acceptere eller slette alle cookies, eller kontrollere det individuelt for hver enkelt hjemmeside. Du kan foretage individuelle indstillinger i dine browserindstillinger. Se hvordan du gør: <a href="http://minecookies.org/cookiehandtering" target="blank">http://minecookies.org/cookiehandtering</a></p>
+
+                  <h3>3. Typer af cookies på vores hjemmesider:</h3>
+                  <ul>
+                    <li>Systemets egne cookies: Det er dem, der sikrer, at alt på hjemmesiden fungerer, som du forventer det</li>
+                    <li>Trafikmålings-cookies: Giver os oplysninger om besøgstal og mulighed for at målrette indhold til dig</li>
+                    <li>Cookies fra indlejrede tjenester: Sikrer funktionaliteten af indlejrede tjenester</li>
+                  </ul>
+
+                  <h3>3.1 Systemets egne cookies</h3>
+                  <p>Så er det nu’s hjemmesider benytter en række cookies til at optimere brugeroplevelsen. Disse cookies sørger blandt andet for at forudfylde formularer med information, som du tidligere har indtastet.</p>
+
+                  <h3>3.2 Trafikmåling og målretning</h3>
+                  <p>Så er det nu’s hjemmesider bruger cookies fra Google Analytics og Adwords til at måle trafikken. Formålet med besøgsstatistikken er at blive klogere på hvor mange mennesker, der besøger hjemmesiderne, hvilken type teknologi, de bruger (f.eks Mac eller Windows), hvor lang tid, de bruger på sitet, hvilke sider de ser på, m.m. Dette hjælper os til løbende at forbedre vores hjemmeside og giver os mulighed for at optimere indholdet til dig. De oplysninger, som Google Analytics cookies indsamler om din brug (trafikdata, herunder din IP-adresse), sendes til og gemmes på Googles servere i USA. Du kan fravælge cookies fra Google Analytics her: <a href="http://tools.google.com/dlpage/gaoptout" target="blank">http://tools.google.com/dlpage/gaoptout</a></p>
+
+
+                  <h3>3.3 Sociale medier</h3>
+                  <p>Vi bruger cookies i forbindelse med vores integration af sociale medier. Formålet er at gøre det muligt for dig at dele vores indhold på disse medier, samt at gøre det muligt for os at målrette annoncer med opfordringer til at deltage i nye handlinger forlængelse af Så er det nu’s formål. Der bliver sat cookies af:
+                  </p>
+                    <ul>
+                      <li>Facebook</li>
+                      <li>Twitter</li>
+                    </ul>
+                  
+
+
+                  <h3>3.4 Indlejrede tjenester</h3>
+                  <p>Vi benytter en række tredjepartstjenester til at vise indlejret (embedded) indhold på hjemmesiden. Formålet er at gøre det muligt at se film, præsentationer, publikationer, udfylde formularer m.m., der ligger på disse tjenester, uden først at forlade Så er det nu’s’ hjemmeside. Så er det nu benytter ikke selv annoncer på hjemmesiden, men nogle af vores indlejrede tjenester kan indeholder annoncer. Der bliver sat cookies af:
+                  </p>
+
+                  <ul>
+                      <li>The Action Network - www.actionnetwork.org</li>
+                    </ul>
+
+
+                  <h3>4. Sådan afviser eller sletter du dine cookies</h3>
+                  <p>Afvisning af cookies: Du kan altid afvise cookies på din computer ved at ændre indstillingerne i din Internet-browser. Hvor du finder indstillingerne afhænger af, hvilken browser du anvender. Du skal dog være opmærksom på, at hvis du gør det, er der mange funktioner og services, du ikke kan bruge, fordi disse funktioner og services forudsætter, at hjemmesiden kan huske de valg, du foretager.
+                  </p>
+                  <p>
+                  Sletning af cookies: Cookies som du tidligere har accepteret, kan efterfølgende slettes nemt. Benytter du en PC /computer med en nyere Internet-browser, kan du slette dine cookies ved at bruge genvejstasterne: 
+                  CTRL + SHIFT + Delete. Virker genvejstasterne ikke, og/eller benytter du en MAC, skal du starte med at finde ud af, hvilken browser du bruger og herefter klikke på det relevante link:<br/>
+                  Husk: Bruger du flere Internet-browsere, skal du slette cookies i dem alle.<br/>
+                  <a href="https://support.microsoft.com/en-us/help/278835/how-to-delete-cookie-files-in-internet-explorer" target="blank">Internet Explorer</a><br/>
+                  <a href="https://support.mozilla.org/da/kb/slet-cookies-fjerne-oplysninger-fra-websteder" target="blank">Mozilla Firefox</a><br/>
+                  <a href="https://support.google.com/chrome/answer/95647?co=GENIE.Platform%3DDesktop&hl=da" target="blank">Google Chrome</a><br/>
+                  <a href="https://help.opera.com/en/latest/web-preferences/" target="blank">Opera</a><br/>
+                  <a href="https://support.apple.com/da-dk/guide/safari/sfri11471/mac" target="blank">Safari</a><br/>
+                  <a href="http://www.macromedia.com/support/documentation/en/flashplayer/help/settings_manager07.html" target="blank">Flash cookies (alle browsere)</a><br/>
+                  <a href="https://support.apple.com/da-dk/HT201265" target="blank">iPhone, iPad og andet fra Apple</a><br/>
+                  <a href="https://support.google.com/chrome/answer/95647?co=GENIE.Platform%3DAndroid&hl=da" target="blank">Telefoner med Android styresystem</a><br/>
+                  </p>
+
+                  <h3>5. Oversigt over Cookies på www.saaerdet.nu</h3>
+                  <p>Følgende cookies bliver sat i din browser på www.saaerdet.nu:</p>
+                  <div className="table-wrapper">
+                  <table>
+                    <tr>
+                      <th>Navn</th>
+                      <th>Udbyder</th>
+                      <th>Formål</th>
+                      <th>Udløb</th>
+                    </tr>
+                    <tr>
+                      <td>_ga</td>
+                      <td>Google</td>
+                      <td>Statistik over antal besøgende og adfærd. Bruges til at skelne brugere ad.</td>
+                      <td>2 år</td>
+                    </tr>
+                    <tr>
+                      <td>_gid</td>
+                      <td>Google</td>
+                      <td>Statistik over antal besøgende og adfærd. Bruges til at skelne brugere ad.</td>
+                      <td>24 timer</td>
+                    </tr>
+                    <tr>
+                      <td>_gat_UA-134595071-1</td>
+                      <td>Google</td>
+                      <td>Begrænser raten af forespørgsler sendt til Google.</td>
+                      <td>1 minut</td>
+                    </tr>
+                    <tr>
+                      <td>_fbp</td>
+                      <td>Facebook</td>
+                      <td>Statistik over besøgende. Bruges til at optimere og målrette vores kommunikation på facebook.</td>
+                      <td>3 måneder</td>
+                    </tr>
+                    <tr>
+                      <td>CookieConsent</td>
+                      <td>Så er det nu</td>
+                      <td>Gemmer dine cookieindstillinger</td>
+                      <td>30 dage</td>
+                    </tr>
+                    
+                  </table>
+                  </div>
+
+
+                  <h3>6. Ejeroplysninger</h3>
+                  <p>Foreningen Så er det nu, København
+                  </p>
+
+
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </Layout>
+);
+
+export default cookiePolicy;
